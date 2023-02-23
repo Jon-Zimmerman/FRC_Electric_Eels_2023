c4567235f92fd43e63da58e66b7d05e45281c6c6
Jon-Zimmerman <32981889+Jon-Zimmerman@users.noreply.github.com>
2023-02-23 03:07:52 -0500

    Auto Code more good

--- src/main/java/frc/robot/autos/exampleAuto2.java
+++ src/main/java/frc/robot/autos/exampleAuto2.java
@@ -21,6 +21,9 @@ import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import edu.wpi.first.wpilibj2.command.Command;
+import java.util.HashMap;
+//import frc.robot.commands.GoToElevatorTop;
 
 import frc.robot.subsystems.intake.Intake;
 
@@ -30,8 +33,14 @@ import frc.robot.subsystems.slider.Slider;
 
 public class exampleAuto2 extends SequentialCommandGroup {
     List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup("Path1", new PathConstraints(4, 3));
-    
+    final HashMap<String, Command> eventMap = new HashMap<String, Command>();
+
     public exampleAuto2(Swerve s_Swerve, Intake intake, Elevator elevator,Slider slider){
+        eventMap.put("ElevatorTop", new InstantCommand(() -> elevator.elevatorTop()));
+        //OR
+        // eventMap.put("ElevatorTop", new GoToEleatorTop());
+        //But I have no idea why you would want to do this
+        eventMap.put("intakeIn", new InstantCommand(() -> intake.intakeIn()));
         addCommands(
             new InstantCommand(() -> s_Swerve.swerveAutoBuilder.fullAuto (pathGroup))
         );
--- src/main/java/frc/robot/subsystems/drive/Swerve.java
+++ src/main/java/frc/robot/subsystems/drive/Swerve.java
@@ -20,9 +20,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.pathplanner.lib.PathConstraints;
-import com.pathplanner.lib.PathPlanner;
-import com.pathplanner.lib.PathPlannerTrajectory;
+
 import com.pathplanner.lib.auto.PIDConstants;
 import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
@@ -59,6 +57,8 @@ public class Swerve extends SubsystemBase {
         resetModulesToAbsolute();
         swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics,
                 Rotation2d.fromDegrees(gyroInputs.yawDegrees), getModulePositions());
+
+
 
         swerveAutoBuilder = new SwerveAutoBuilder(
                 this::getPose,
